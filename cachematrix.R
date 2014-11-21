a39423a379e31c41113c357f886774b93c426cba
Kirby <ckarinder@aol.com>
2014-11-21 16:23:51 -0600

    attempt 2 to commit assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  calcinverse <- NULL
+  set <- function(passmatrix) {
+    mymatrix <<- passmatrix
+    calcinverse <<- NULL
+  }
+  get <- function() mymatrix
+  setinverse <- function(passinverse) {
+    calcinverse <<- passinverse
+  }
+  getinverse <- function() calcinverse
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  calcinverse <- x$getinverse()
+  if(!is.null(calcinverse)){
+    message("getting cached data")
+    return(calcinverse)
+  }
+  mymatrix <- x$get()
+  calcinverse <- solve(mymatrix, ...)
+  x$setinverse(calcinverse)
+  calcinverse
         ## Return a matrix that is the inverse of 'x'
 }
